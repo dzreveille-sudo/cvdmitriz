106a7ce9c44a4d05f2b416a52b2dcca55678c27e
dmitrizolotov <dzreveille@gmail.com>
2026-04-03 15:24:50 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -166,7 +166,15 @@ useEffect(() => {
             <a href="#certificates" className="rounded-full border border-[#8a8a84] bg-[#ecece7] px-4 py-2 text-sm text-[#353531] transition duration-300 hover:-translate-y-0.5 hover:bg-white/50 hover:shadow-md">
               Сертификаты
             </a>
+            
           </div>
+          <a
+        href="/resume.docx"
+        download="Дмитрий_Золотов_Резюме.docx"
+        className="rounded-full border border-orange-500 bg-orange-500 px-4 py-2 text-sm text-white transition duration-300 hover:-translate-y-0.5 hover:bg-orange-600 hover:shadow-md"
+      >
+        Скачать резюме .docx
+      </a>
         </nav>
       </header>
 
