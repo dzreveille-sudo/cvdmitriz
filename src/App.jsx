f760b095f5dbd955e959f35c39ea522e6e7f0da2
dmitrizolotov <dzreveille@gmail.com>
2026-04-03 15:26:23 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -166,7 +166,6 @@ useEffect(() => {
             <a href="#certificates" className="rounded-full border border-[#8a8a84] bg-[#ecece7] px-4 py-2 text-sm text-[#353531] transition duration-300 hover:-translate-y-0.5 hover:bg-white/50 hover:shadow-md">
               Сертификаты
             </a>
-            
           </div>
           <a
         href="/resume.docx"
